003a7477356d91662c3e8e9d27dcf3f6d2b32e02
hhssome <artempavel-terehove@mail.ru>
2023-02-12 16:48:47 +0300

    New containers and factories

--- src/di/containers.py
+++ src/di/containers.py
@@ -1,6 +1,9 @@
 from rodi import Container
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
 
-from src.di.factories import provide_db_settings
+from src.di.factories import provide_db_settings, provide_pool_sessions
+from src.infrastructure.db.base import create_engine
 from src.settings import DBSettings
 
 
@@ -8,5 +11,7 @@ def get_container() -> Container:
     container = Container()
 
     container.add_scoped_by_factory(provide_db_settings, DBSettings)
+    container.add_singleton_by_factory(create_engine, AsyncEngine)
+    container.add_scoped_by_factory(provide_pool_sessions, sessionmaker)
 
     return container
--- src/di/factories.py
+++ src/di/factories.py
@@ -1,8 +1,17 @@
 from envdataclass import from_file
 from rodi import ActivationScope
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
 
+from src.infrastructure.db.base import create_pool
 from src.settings import DBSettings
 
 
 def provide_db_settings(scope: ActivationScope) -> DBSettings:
     return from_file(DBSettings, '.env')
+
+
+def provide_pool_sessions(scope: ActivationScope) -> sessionmaker:
+    engine = scope.provider.get(AsyncEngine)
+
+    return create_pool(engine)
