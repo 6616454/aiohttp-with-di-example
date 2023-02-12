45ae9d2b6e4848c64a2b5c0c57cab0aee3c4e0ca
hhssome <artempavel-terehove@mail.ru>
2023-02-12 16:37:48 +0300

    Added first dependency by factory

--- src/di/containers.py
+++ src/di/containers.py
@@ -1,7 +1,12 @@
 from rodi import Container
 
+from src.di.factories import provide_db_settings
+from src.settings import DBSettings
+
 
 def get_container() -> Container:
     container = Container()
+
+    container.add_scoped_by_factory(provide_db_settings, DBSettings)
 
     return container
