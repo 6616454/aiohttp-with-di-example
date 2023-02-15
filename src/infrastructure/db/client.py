10a001e6c3843e8804b97f37dc2ee2401fcc0bf5
hhssome <artempavel-terehove@mail.ru>
2023-02-15 18:04:55 +0300

    type-hint

--- src/infrastructure/db/client.py
+++ src/infrastructure/db/client.py
@@ -2,5 +2,5 @@ class SomeDatabaseClient:
     def __init__(self, bar: str):
         self.foo = bar
 
-    def get_by_id(self, model: dict, id_: int):
+    def get_by_id(self, model: dict, id_: int) -> dict:
         return model.get(id_)
