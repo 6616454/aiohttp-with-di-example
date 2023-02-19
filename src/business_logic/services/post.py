21acb39736b76742f924ffeff3fe141080e8603d
hhssome <artempavel-terekhovy@mail.ru>
2023-02-19 20:05:09 +0300

    Upd. type hints

--- src/business_logic/services/post.py
+++ src/business_logic/services/post.py
@@ -1,12 +1,12 @@
 from src.business_logic.dto.post import PostDTO
 from src.infrastructure.db.client import SomeDatabaseClient
-from src.infrastructure.db.models.post import SomePostModel
+from src.infrastructure.db.models.post import PostModel
 
 
 class PostService:
     def __init__(self, db_client: SomeDatabaseClient):
         self._db = db_client
 
-    def get_post_by_id(self, id_: str):
-        post = self._db.get_by_id(SomePostModel, id_)
+    def get_post_by_id(self, id_: int):
+        post = self._db.get_by_id(PostModel, id_)
         return PostDTO(title=post['title'], body=post['body'])
