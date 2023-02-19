c3467a2d2d6485e06f6b7b7e4be30cbd95de363c
hhssome <artempavel-terekhovy@mail.ru>
2023-02-19 20:04:37 +0300

    Delete type-hint

--- src/di/__init__.py
+++ src/di/__init__.py
@@ -1,8 +1,6 @@
-from rodi import Container
-
 from src.di.containers import get_container
 from src.presentation.api import Application
 
 
-def setup_di(app: Application) -> Container:
+def setup_di(app: Application) -> None:
     app.container = get_container()
